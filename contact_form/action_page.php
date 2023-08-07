38ba0d1f55098b7ed4d069b78c27538568235bbc
mckennawilds <mckenna.wilds@gmail.com>
2023-08-07 11:05:53 -0700

    swapped Contact tab with Email

--- contact_form/action_page.php
+++ contact_form/action_page.php
@@ -1,10 +1,10 @@
-Hi <?php echo htmlspecialchars($_POST['name']); ?>.
+Hi <?php echo htmlspecialchars($GET['fname']); ?>.
 Thank you for your message.
 
 
 <!--
 	https://www.php.net/manual/en/tutorial.forms.php
-	
+
 example
 Hi <?php echo htmlspecialchars($_POST['name']); ?>.
 You are <?php echo (int)$_POST['age']; ?> years old.
